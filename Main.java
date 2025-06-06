6b8883da4fca998025bdb4f7622add605f9697bd
Salvatore Barretta <barrettasalvatore@outlook.it>
2025-06-06 23:31:42 +0200

    feat : testing lettura animali

--- Main.java
+++ Main.java
@@ -1,31 +1,20 @@
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.List;
+import models.*;
+import dao.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-        // Mappa con chiave composta da data+ora
-        Map<String, slot> slotMap = new HashMap<>();
+        AnimaleDAO dao = new AnimaleDAO();
 
-        // Creazione di uno slot
-        Date data = sdf.parse("2025-06-04");
-        slot slot1 = new slot(data, 10); // ore 10
-
-        // Inserimento nella mappa
-        slotMap.put(slot1.getChiave(), slot1);
-
-        // Controllo se lo slot è occupato
-        String chiave = "2025-06-04-10";
-        if (slotMap.containsKey(chiave)) {
-            slot s = slotMap.get(chiave);
-            System.out.println(s.toString());
-
-            // Modifica stato
-            s.setOccupato(true);
-            System.out.println("Dopo modifica: " + s);
-        } else {
-            System.out.println("Slot non trovato.");
+        try {
+            // Recupero
+            List<Animale> animali = dao.readAnimale(null); // Passa null per ottenere tutti gli animali
+            for (Animale a : animali) {
+                System.out.println(a.getNome() + " " + a.getRazza() + " " + a.getColore() + " " + a.getDataNascita());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
