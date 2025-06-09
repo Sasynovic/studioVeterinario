1f7bde9feda075e2ef0ba2ce4a6a759dbe128b09
Salvatore Barretta <barrettasalvatore@outlook.it>
2025-06-09 17:55:21 +0200

    feat : aggiunti ulteriori campi al ritorno di loginresult

--- control/LoginResult.java
+++ control/LoginResult.java
@@ -4,11 +4,17 @@ public class LoginResult {
     private boolean success;
     private int tipoUtente;
     private String message;
+    private String nome;
+    private String cognome;
+    private String username;
 
-    public LoginResult(boolean success, int tipoUtente, String message) {
+    public LoginResult(boolean success, int tipoUtente, String message, String nome, String cognome, String username) {
         this.success = success;
         this.tipoUtente = tipoUtente;
         this.message = message;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.username = username;
     }
 
     public boolean isSuccess() {
@@ -19,6 +25,15 @@ public class LoginResult {
     }
     public String getMessage() {
         return message;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getCognome() {
+        return cognome;
+    }
+    public String getUsername() {
+        return username;
     }
 }
 
